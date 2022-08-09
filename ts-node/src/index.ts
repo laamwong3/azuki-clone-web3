45ada5addf8a4c018ee9e3be06e1789519af9b44
Laam Wong <laamwong3@gmail.com>
2022-08-10 07:12:56 +0800

    fetch api

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,91 @@
-// import client from "https";
-import fs from "fs";
+import client from "https";
+import * as fs from "fs";
+import nodeFetch from "node-fetch";
+import dotenv from "dotenv";
 
-//fetch azuki
+dotenv.config({ path: "../.env" });
 
-const azukiContractAddress = "0xED5AF388653567Af2F388E6224dC7C4b3241C544";
+//fetch azuki metadata
+//download 100 images for project
 
-const fetchMetadata = async () => {};
+const contractAddress = "0xED5AF388653567Af2F388E6224dC7C4b3241C544"; //Azuki
 
-(async () => {})().catch((e) => console.log(e));
+const frontendPath = "../../constants/azuki/images";
+const backendDataPath = "./data";
+const backendImagePath = "./images";
+
+const fetchMetadata = async () => {
+  const url = `https://deep-index.moralis.io/api/v2/nft/${contractAddress}?chain=eth&format=decimal&limit=10`;
+  nodeFetch(url, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "X-API-Key": process.env.X_API_KEY ?? "",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      fs.writeFileSync(
+        `${backendDataPath}/rawData.json`,
+        JSON.stringify(data, null, 2)
+      );
+    })
+    .catch((e) => console.log(e));
+};
+
+const retrieveTokenUri = async () => {
+  const rawMetadata = await import("./data/rawData.json");
+  let tempObj = {} as { data: string[] };
+  tempObj.data = [];
+
+  rawMetadata.result.map((data) => {
+    tempObj.data.push(JSON.parse(data.metadata));
+    // tempArr.push(JSON.parse(data.metadata));
+  });
+  fs.writeFileSync(
+    `${backendDataPath}/tokenUri.json`,
+    JSON.stringify(tempObj, null, 2)
+  );
+};
+
+const downloadImages = async () => {
+  const tokenUri = await import("./data/tokenUri.json");
+
+  tokenUri.data.map((data, index) => {
+    client.get(data.image, (res) => {
+      res.pipe(fs.createWriteStream(`${backendImagePath}/${index}.png`));
+    });
+  });
+
+  //   client.get(tokenUri.data[0].image, (res) => {
+  //     res.pipe(fs.createWriteStream(`${backendImagePath}/test.png`));
+  //   });
+};
+
+const clean = async () => {
+  if (fs.existsSync(backendImagePath)) {
+    fs.rm(backendImagePath, { recursive: true, force: true }, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        fs.mkdirSync(backendImagePath);
+      }
+    });
+  }
+  if (fs.existsSync(backendDataPath)) {
+    fs.rm(backendDataPath, { recursive: true, force: true }, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        fs.mkdirSync(backendDataPath);
+      }
+    });
+  }
+};
+
+(async () => {
+  //   await clean();
+  await fetchMetadata();
+  await retrieveTokenUri();
+  //   await downloadImages();
+})().catch((e) => console.log(e));
