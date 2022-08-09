bf5f24675a960f506bca5d33ec8fe2ae8ec87dc9
Laam Wong <laamwong3@gmail.com>
2022-08-10 04:55:03 +0800

    project creation

---
+++ ts-node/src/index.ts
@@ -0,0 +1,10 @@
+// import client from "https";
+import fs from "fs";
+
+//fetch azuki
+
+const azukiContractAddress = "0xED5AF388653567Af2F388E6224dC7C4b3241C544";
+
+const fetchMetadata = async () => {};
+
+(async () => {})().catch((e) => console.log(e));
