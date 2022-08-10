9bcfb5ed5c6f23138007607d26591f73b07773f7
Laam Wong <laamwong3@gmail.com>
2022-08-10 12:11:44 +0800

    setup frontend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import darkTheme from "../configs/darkTheme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
